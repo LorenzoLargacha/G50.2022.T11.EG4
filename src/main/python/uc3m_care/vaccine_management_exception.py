f8352dfeeb803409048284b65b03232ce6090300
SonsolesMolinaAbad <100432073@alumnos.uc3m.es>
2022-04-27 00:07:22 +0200

    Tipado: métodos clase VaccineManagementException

--- src/main/python/uc3m_care/vaccine_management_exception.py
+++ src/main/python/uc3m_care/vaccine_management_exception.py
@@ -2,15 +2,15 @@
 
 class VaccineManagementException(Exception):
     """Personalised exception for Vaccine Management"""
-    def __init__(self, message):
+    def __init__(self, message: str) -> None:
         self.__message = message
         super().__init__(self.message)
 
     @property
-    def message(self):
+    def message(self) -> str:
         """gets the message value"""
         return self.__message
 
     @message.setter
-    def message(self, value):
+    def message(self, value: str) -> None:
         self.__message = value
