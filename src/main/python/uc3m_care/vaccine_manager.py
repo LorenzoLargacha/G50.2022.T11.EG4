2e635f0e0f84128599eff643e8899b7133a571aa
SonsolesMolinaAbad <100432073@alumnos.uc3m.es>
2022-04-26 23:36:28 +0200

    Tipado: método save_store

--- src/main/python/uc3m_care/vaccine_manager.py
+++ src/main/python/uc3m_care/vaccine_manager.py
@@ -24,7 +24,7 @@ class VaccineManager:
             raise VaccineManagementException("date_signature format is not valid")
 
     @staticmethod
-    def save_store(data):
+    def save_store(data: VaccinePatientRegister) -> True:
         """Medthod for saving the patients store"""
         file_store = JSON_FILES_PATH + "store_patient.json"
         #first read the file
