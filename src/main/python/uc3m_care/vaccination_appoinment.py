2cb55dd1fc0a2cf201863342bf90fac378c12055
SonsolesMolinaAbad <100432073@alumnos.uc3m.es>
2022-04-27 00:04:31 +0200

    Tipado: métodos clase VaccinationAppoinment

--- src/main/python/uc3m_care/vaccination_appoinment.py
+++ src/main/python/uc3m_care/vaccination_appoinment.py
@@ -23,55 +23,55 @@ class VaccinationAppoinment():
         self.__date_signature = self.vaccination_signature
 
 
-    def __signature_string(self):
+    def __signature_string(self) -> str:
         """Composes the string to be used for generating the key for the date"""
         return "{alg:" + self.__alg +",typ:" + self.__type +",patient_sys_id:" + \
                self.__patient_sys_id + ",issuedate:" + self.__issued_at.__str__() + \
                ",vaccinationtiondate:" + self.__appoinment_date.__str__() + "}"
 
     @property
-    def patient_id(self):
+    def patient_id(self) -> str:
         """Property that represents the guid of the patient"""
         return self.__patient_id
     @patient_id.setter
-    def patient_id(self, value):
+    def patient_id(self, value: str) -> None:
         self.__patient_id = value
 
     @property
-    def patient_sys_id(self):
+    def patient_sys_id(self) -> str:
         """Property that represents the patient_sys_id of the patient"""
         return self.__patient_sys_id
     @patient_sys_id.setter
-    def patient_sys_id(self, value):
+    def patient_sys_id(self, value: str) -> None:
         self.__patient_sys_id = value
 
     @property
-    def phone_number(self):
+    def phone_number(self) -> str:
         """Property that represents the phone number of the patient"""
         return self.__phone_number
     @phone_number.setter
-    def phone_number(self, value):
+    def phone_number(self, value: str) -> None:
         self.__phone_number = value
 
     @property
-    def vaccination_signature(self):
+    def vaccination_signature(self) -> str:
         """Returns the sha256 signature of the date"""
         return hashlib.sha256(self.__signature_string().encode()).hexdigest()
 
     @property
-    def issued_at(self):
+    def issued_at(self) -> float:
         """Returns the issued at value"""
         return self.__issued_at
     @issued_at.setter
-    def issued_at(self, value):
+    def issued_at(self, value: float) -> None:
         self.__issued_at = value
 
     @property
-    def appoinment_date(self):
+    def appoinment_date(self) -> float:
         """Returns the vaccination date"""
         return self.__appoinment_date
 
     @property
-    def date_signature(self):
+    def date_signature(self) -> str:
         """Returns the SHA256 """
         return self.__date_signature
