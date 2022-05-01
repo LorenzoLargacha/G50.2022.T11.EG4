df78f9a5b076808a1c53f011bcc620280d66c929
LorenzoLargacha <100432129@alumnos.uc3m.es>
2022-05-02 00:18:52 +0200

    Palabras y números mágicos: creación de constantes para la clase VaccineLog

--- src/main/python/uc3m_care/data/vaccine_log.py
+++ src/main/python/uc3m_care/data/vaccine_log.py
@@ -5,6 +5,11 @@ from uc3m_care.storage.appointment_json_store import AppointmentJsonStore
 from uc3m_care.exception.vaccine_management_exception import VaccineManagementException
 
 class VaccineLog:
+    """Clase que representa la firma y fecha de vacunación de un paciente"""
+    KEY_LABEL_APPOINMENT_DATE = "_VaccinationAppoinment__appoinment_date"
+    __ERROR_MESSAGE_DATE_SIGNATURE_NOT_FOUND = "date_signature is not found"
+    __ERROR_MESSAGE_NOT_THE_DATE = "Today is not the date"
+
     def __init__(self, date_signature: str) -> None:
         self.__date_signature = DateSignature(date_signature).value
         justnow = datetime.utcnow()
@@ -28,7 +33,7 @@ class VaccineLog:
 
     def check_date(self):
         item = self.check_date_signature()
-        date_time = item["_VaccinationAppoinment__appoinment_date"]
+        date_time = item[self.KEY_LABEL_APPOINMENT_DATE]
         self.is_the_date(date_time)
 
     def check_date_signature(self):
@@ -36,12 +41,12 @@ class VaccineLog:
         my_store_date = AppointmentJsonStore()
         item = my_store_date.find_date_signature(self.__date_signature)
         if item is None:
-            raise VaccineManagementException("date_signature is not found")
+            raise VaccineManagementException(self.__ERROR_MESSAGE_DATE_SIGNATURE_NOT_FOUND)
         return item
 
     def is_the_date(self, date_time):
         today = datetime.today().date()
         date_patient = datetime.fromtimestamp(date_time).date()
         if date_patient != today:
-            raise VaccineManagementException("Today is not the date")
+            raise VaccineManagementException(self.__ERROR_MESSAGE_NOT_THE_DATE)
 
