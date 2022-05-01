73abe3a147cdcd75f20b17bd0c87e99f455b7ff2
LorenzoLargacha <100432129@alumnos.uc3m.es>
2022-05-02 01:03:32 +0200

    Palabras y números mágicos: creación de constantes para la clase VaccineJsonStore

--- src/main/python/uc3m_care/storage/vaccine_json_store.py
+++ src/main/python/uc3m_care/storage/vaccine_json_store.py
@@ -7,11 +7,13 @@ from uc3m_care.cfg.vaccine_manager_config import JSON_FILES_PATH
 class VaccineJsonStore(JsonStore):
     _FILE_PATH = JSON_FILES_PATH + "store_vaccine.json"
 
+    __ERROR_MESSAGE_INVALID_OBJECT = "Invalid vaccine log object"
+
     def __init__(self):
         pass
 
     def add_item(self, vaccine_log: VaccineLog) -> None:
         """Saves the vaccine into a file"""
         if not isinstance(vaccine_log, VaccineLog):
-            raise VaccineManagementException("Invalid vaccine log object")
+            raise VaccineManagementException(self.__ERROR_MESSAGE_INVALID_OBJECT)
         super().add_item(vaccine_log)
