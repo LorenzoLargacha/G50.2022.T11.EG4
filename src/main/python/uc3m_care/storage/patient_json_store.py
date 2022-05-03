6ffc3f23a5dae5eac73921fb3d5127c300394df9
LorenzoLargacha <100432129@alumnos.uc3m.es>
2022-05-03 12:13:59 +0200

    Patron Singleton: aplicamos patron singleton a la clase PatientJsonStore y creamos su correspondiente test en tests_singleton_tests

--- src/main/python/uc3m_care/storage/patient_json_store.py
+++ src/main/python/uc3m_care/storage/patient_json_store.py
@@ -7,42 +7,52 @@ from uc3m_care.cfg.vaccine_manager_config import JSON_FILES_PATH
 
 class PatientJsonStore(JsonStore):
     """Clase hija de JsonStore con los atributos para store_patient"""
-    _FILE_PATH = JSON_FILES_PATH + "store_patient.json"
-    _ID_FIELD = "_VaccinePatientRegister__patient_id"
 
-    __ERROR_MESSAGE_INVALID_OBJECT = "Invalid vaccine patient register object"
-    __ERROR_MESSAGE_PATIENT_ID_REGISTERED = "patien_id is registered in store_patient"
+    class __PatientJsonStore(JsonStore):
+        """Clase privada, patron singleton"""
+        _FILE_PATH = JSON_FILES_PATH + "store_patient.json"
+        _ID_FIELD = "_VaccinePatientRegister__patient_id"
 
-    def __init__(self):
-        pass
+        __ERROR_MESSAGE_INVALID_OBJECT = "Invalid vaccine patient register object"
+        __ERROR_MESSAGE_PATIENT_ID_REGISTERED = "patien_id is registered in store_patient"
 
-    def save_store_patient(self, patient: VaccinePatientRegister) -> True:
-        """Saves a patient into a file"""
-        if not isinstance(patient, VaccinePatientRegister):
-            raise VaccineManagementException(self.__ERROR_MESSAGE_INVALID_OBJECT)
+        def __init__(self):
+            pass
 
-        found = False
-        # Buscamos el patient_id
-        item = self.find_item(patient.patient_id)
-        # Si lo encontramos, buscamos el registration_type y el full_name
-        if item is not None:
-            if (item[VaccinePatientRegister.KEY_LABEL_REGISTRATION_TYPE] == patient.vaccine_type) and \
-                    (item[VaccinePatientRegister.KEY_LABEL_FULL_NAME] == patient.full_name):
-                found = True
+        def save_store_patient(self, patient: VaccinePatientRegister) -> True:
+            """Saves a patient into a file"""
+            if not isinstance(patient, VaccinePatientRegister):
+                raise VaccineManagementException(self.__ERROR_MESSAGE_INVALID_OBJECT)
 
-        if found is False:
-            self.add_item(patient)
+            found = False
+            # Buscamos el patient_id
+            item = self.find_item(patient.patient_id)
+            # Si lo encontramos, buscamos el registration_type y el full_name
+            if item is not None:
+                if (item[VaccinePatientRegister.KEY_LABEL_REGISTRATION_TYPE] == patient.vaccine_type) and \
+                        (item[VaccinePatientRegister.KEY_LABEL_FULL_NAME] == patient.full_name):
+                    found = True
 
-        if found is True:
-            raise VaccineManagementException(self.__ERROR_MESSAGE_PATIENT_ID_REGISTERED)
+            if found is False:
+                self.add_item(patient)
 
-        return True
+            if found is True:
+                raise VaccineManagementException(self.__ERROR_MESSAGE_PATIENT_ID_REGISTERED)
 
-    def find_patient_store(self, patient_system_id: str) -> VaccinePatientRegister:
-        """Looks for a patient_system_id in store_patient"""
-        with open(self._FILE_PATH, "r", encoding="utf-8", newline=""):
-            self._ID_FIELD = VaccinePatientRegister.KEY_LABEL_PATIENT_SYSTEM_ID
-            item_found = self.find_item(patient_system_id)
-            self._ID_FIELD = VaccinePatientRegister.KEY_LABEL_PATIENT_ID
+            return True
 
-        return item_found
+        def find_patient_store(self, patient_system_id: str) -> VaccinePatientRegister:
+            """Looks for a patient_system_id in store_patient"""
+            with open(self._FILE_PATH, "r", encoding="utf-8", newline=""):
+                self._ID_FIELD = VaccinePatientRegister.KEY_LABEL_PATIENT_SYSTEM_ID
+                item_found = self.find_item(patient_system_id)
+                self._ID_FIELD = VaccinePatientRegister.KEY_LABEL_PATIENT_ID
+
+            return item_found
+
+    __instance = None
+
+    def __new__(cls):
+        if not PatientJsonStore.__instance:
+            PatientJsonStore.__instance = PatientJsonStore.__PatientJsonStore()
+        return PatientJsonStore.__instance
--- src/unittest/python/test_singleton_tests.py
+++ src/unittest/python/test_singleton_tests.py
@@ -1,12 +1,24 @@
 """Module for testing singleton"""
 import unittest
 
+from uc3m_care.storage.patient_json_store import PatientJsonStore
 from uc3m_care.storage.appointment_json_store import AppointmentJsonStore
 from uc3m_care.storage.vaccine_json_store import VaccineJsonStore
 
 
 class SingletonTests(unittest.TestCase):
     """Class for testing singleton"""
+    def test_singleton_patient_json_store(self):
+        """Method for testing singleton on PatientJsonStore"""
+        my_store_1 = PatientJsonStore()
+        my_store_2 = PatientJsonStore()
+        my_store_3 = PatientJsonStore()
+        my_store_4 = PatientJsonStore()
+
+        self.assertEqual(my_store_1, my_store_2)
+        self.assertEqual(my_store_1, my_store_3)
+        self.assertEqual(my_store_1, my_store_4)
+
     def test_singleton_appointment_json_store(self):
         """Method for testing singleton on AppointmentJsonStore"""
         my_store_1 = AppointmentJsonStore()
